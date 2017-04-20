f173cc9ccf69c5dce7e8799380ec2adede485b8d
zt971064178 <zhang.tian20080808@163.com>
2017-04-20 21:27:33 +0800

    springcloud断路器

---
+++ springcloud-breaker/springcloud-eureka-feign/src/main/java/cn/itcast/zt/service/ComputeClient2.java
@@ -0,0 +1,21 @@
+package cn.itcast.zt.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by zhangtian on 2017/4/20.
+ */
+@FeignClient(value = "compute-service", fallback = ComputeClientHystrix.class)
+public interface ComputeClient2 {
+
+    /**
+     * 调用远程服务
+     * @param a
+     * @param b
+     * @return
+     */
+    @GetMapping(value = "/add")
+    public Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) ;
+}
--- springcloud-breaker/springcloud-eureka-feign/src/main/java/cn/itcast/zt/web/ConsumerController.java
+++ springcloud-breaker/springcloud-eureka-feign/src/main/java/cn/itcast/zt/web/ConsumerController.java
@@ -1,7 +1,9 @@
 package cn.itcast.zt.web;
 
 import cn.itcast.zt.service.ComputeClient;
+import cn.itcast.zt.service.ComputeClient2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,9 @@ public class ConsumerController {
     @Autowired
     private ComputeClient computeClient ;
 
+    @Autowired
+    private ComputeClient2 computeClient2 ;
+
     /**
      * 调用远程服务就像调用用本地服务一样
      * @return
@@ -20,5 +25,14 @@ public class ConsumerController {
     @GetMapping(value = "/addFeign")
     public Integer add() {
         return computeClient.add(10,90) ;
+    }
+
+    /**
+     * 调用远程服务就像调用用本地服务一样
+     * @return
+     */
+    @GetMapping(value = "/addFeign2")
+    public Integer add2() {
+        return computeClient2.add(10,90) ;
     }
 }
