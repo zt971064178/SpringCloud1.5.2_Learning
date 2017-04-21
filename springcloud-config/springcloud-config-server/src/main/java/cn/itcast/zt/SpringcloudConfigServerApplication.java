4fd9564dc2b2215ce3c5c57f9ce2d0dfd273d58b
zhangtian <zhang.tian20080808>
2017-04-21 15:26:49 +0800

    SpringCloud分布式配置中心

--- springcloud-config/springcloud-config-server/src/main/java/cn/itcast/zt/SpringcloudConfigServerApplication.java
+++ springcloud-config/springcloud-config-server/src/main/java/cn/itcast/zt/SpringcloudConfigServerApplication.java
@@ -2,10 +2,11 @@ package cn.itcast.zt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 // 添加@EnableConfigServer注解，开启Config Server
-@EnableConfigServer
+@EnableCon
 public class SpringcloudConfigServerApplication {
 
 	public static void main(String[] args) {
