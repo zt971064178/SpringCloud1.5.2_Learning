24b55af7b578551cbef19751e9679fd40b085528
zt971064178 <zhang.tian20080808@163.com>
2017-04-21 21:49:12 +0800

    springcloud分布式配置中心

--- springcloud-config/springcloud-config-client/src/main/java/cn/itcast/zt/web/TestController.java
+++ springcloud-config/springcloud-config-client/src/main/java/cn/itcast/zt/web/TestController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
  * 如果配置中心的from的值是一个数组，取得是最新的一个版本
  * 注意：当配置文件中内容被修改之后，服务端在一定时间之后会自动更新
  * 客户端需要调用：http://127.0.0.1:7002/refresh(注：必须是POST请求)刷新数据之后，配置属性在随之更改
+ *
+ * 1. config-client的pom.xml中新增spring-boot-starter-actuator监控模块，其中包含了/refresh刷新API。
+ * 2. git中配置数据修改后，config-client中执行http://localhost:18443/refresh 即可实现client端配置数据更新
  * Created by zhangtian on 2017/4/21.
  */
 @RestController
