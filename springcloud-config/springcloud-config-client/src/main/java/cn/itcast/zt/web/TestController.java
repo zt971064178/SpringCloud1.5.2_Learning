0b818c849f1a1450f305ff3a36e65019cdc81901
zt971064178 <zhang.tian20080808@163.com>
2017-04-21 21:48:36 +0800

    springcloud分布式配置中心

--- springcloud-config/springcloud-config-client/src/main/java/cn/itcast/zt/web/TestController.java
+++ springcloud-config/springcloud-config-client/src/main/java/cn/itcast/zt/web/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * http://127.0.0.1:7002/from
  * 如果配置中心的from的值是一个数组，取得是最新的一个版本
+ * 注意：当配置文件中内容被修改之后，服务端在一定时间之后会自动更新
+ * 客户端需要调用：http://127.0.0.1:7002/refresh(注：必须是POST请求)刷新数据之后，配置属性在随之更改
  * Created by zhangtian on 2017/4/21.
  */
 @RestController
