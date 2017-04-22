f355e46555665efe1c2f99bb5464457288c6e087
zt971064178 <zhang.tian20080808@163.com>
2017-04-22 08:58:50 +0800

    springcloud高可用的分布式配置中心

--- springcloud-config-ha/springcloud-config-client-eureka/src/main/java/cn/itcast/zt/SpringcloudConfigClientEurekaApplication.java
+++ springcloud-config-ha/springcloud-config-client-eureka/src/main/java/cn/itcast/zt/SpringcloudConfigClientEurekaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 增加@EnableDiscoveryClient注解，用来发现config-server服务，利用其来加载应用配置
+ * http://blog.didispace.com/springcloud4-2/  该博客中包含如何刷新git提交的配置中心的变化
  */
 @SpringBootApplication
 @EnableDiscoveryClient
