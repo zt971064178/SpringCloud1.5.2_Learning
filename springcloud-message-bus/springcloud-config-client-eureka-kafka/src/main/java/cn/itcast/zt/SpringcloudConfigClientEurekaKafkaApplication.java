f292f4744d6e3468094c39e024f335b4ed2bb296
zt971064178 <zhang.tian20080808@163.com>
2017-04-22 17:44:44 +0800

    springcloud消息总线

---
+++ springcloud-message-bus/springcloud-config-client-eureka-kafka/src/main/java/cn/itcast/zt/SpringcloudConfigClientEurekaKafkaApplication.java
@@ -0,0 +1,12 @@
+package cn.itcast.zt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigClientEurekaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigClientEurekaKafkaApplication.class, args);
+	}
+}
---
+++ springcloud-message-bus/springcloud-config-server-eureka-kafka/src/main/java/cn/itcast/zt/SpringcloudConfigServerEurekaKafkaApplication.java
@@ -0,0 +1,12 @@
+package cn.itcast.zt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigServerEurekaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigServerEurekaKafkaApplication.class, args);
+	}
+}
