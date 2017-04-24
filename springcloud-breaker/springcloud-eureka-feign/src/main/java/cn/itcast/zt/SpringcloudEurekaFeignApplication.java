8b5d62a36ae30278e0f4bbe8a83e1e5bea05a8fe
zhangtian <zhang.tian20080808>
2017-04-24 16:33:20 +0800

    SpringCloud断路器Hystrix

--- springcloud-breaker/springcloud-eureka-feign/src/main/java/cn/itcast/zt/SpringcloudEurekaFeignApplication.java
+++ springcloud-breaker/springcloud-eureka-feign/src/main/java/cn/itcast/zt/SpringcloudEurekaFeignApplication.java
@@ -7,9 +7,13 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * 通过feign实现的服务单元，依赖compute-service的服务，端口3334
+ * @FeignClient中的fallback : 指定回调类。
+ * @EnableFeignClients : 开启 Fegin 功能（包括 Hystrix ）。
+ * @EnableHystrixDashboard : 开启 Hystrix Dashboard 监控视图
  */
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 @SpringBootApplication
 //@SpringCloudApplication
 public class SpringcloudEurekaFeignApplication {
