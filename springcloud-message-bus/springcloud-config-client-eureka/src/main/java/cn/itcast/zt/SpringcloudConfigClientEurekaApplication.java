ed8343863347fa91d839084a0e47dbe9bbab4486
zhangtian <zhang.tian20080808>
2017-04-24 10:51:24 +0800

    SpringCloud消息总线  rabbitMQ

--- springcloud-message-bus/springcloud-config-client-eureka/src/main/java/cn/itcast/zt/SpringcloudConfigClientEurekaApplication.java
+++ springcloud-message-bus/springcloud-config-client-eureka/src/main/java/cn/itcast/zt/SpringcloudConfigClientEurekaApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 需要安装RabbitMQ
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SpringcloudConfigClientEurekaApplication {
