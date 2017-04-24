c0acc0a51356a2539d62a1b35e0e6dbb218bcda2
zhangtian <zhang.tian20080808>
2017-04-24 10:44:41 +0800

    SpringCloud消息总线  rabbitMQ

--- springcloud-message-bus/springcloud-config-client-eureka/src/main/java/cn/itcast/zt/SpringcloudConfigClientEurekaApplication.java
+++ springcloud-message-bus/springcloud-config-client-eureka/src/main/java/cn/itcast/zt/SpringcloudConfigClientEurekaApplication.java
@@ -2,8 +2,10 @@ package cn.itcast.zt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringcloudConfigClientEurekaApplication {
 
 	public static void main(String[] args) {
